ebd4a907cf228a4020ebd54c132bbace8c436ba7
hablum <38866062+hablum@users.noreply.github.com>
2019-01-08 10:27:16 -0500

    Update madlib.js

--- madlib.js
+++ madlib.js
@@ -1,2 +1,25 @@
 var data;
 
+var txt = '$$EXCLAMATIONS$$! they said as they jumped into their $$NOUN$$ and flew off with their $$ADJECTIVE$$ $$PLURALNOUNS$$.';
+
+function setup() {
+  noCanvas();
+	
+	Tabletop.init({
+		key:
+		callback: gotData,
+		simpleSheet: true
+	}
+								
+	var button = createButton('generateMadlib');
+	button.mousePressed(generate);
+}
+
+function generate() {
+	console.log('generate');
+}
+
+function gotData(data, tabletop) {
+  console.log(data)
+		data = stuff;
+}
